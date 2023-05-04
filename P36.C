7bacdf351406c346901825bc3943a7f676a36544
priyanka-250 <131867826+priyanka-250@users.noreply.github.com>
2023-05-04 02:49:18 -0700

    Add files via upload

---
+++ P36.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b;
+a=10;
+b=20;
+a=a+b;
+b=a-b;
+a=a-b;
+
+clrscr();
+printf("value of a=%d",a);
+printf("\nvalue ofb=%d",b);
+
+getch();
+}
---
+++ P37.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+int a,b,c;
+a=30;
+b=20;
+c=a;
+a=b;
+b=c;
+clrscr();
+printf("value ofa=%d",a);
+printf("\nvalue ofb=%d",b);
+  getch();
+
+  }
